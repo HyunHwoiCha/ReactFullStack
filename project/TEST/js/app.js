e5e76dd01e69878dc2abe240306701fe3a57ed4b
Ca_String <considerate76@gmail.com>
2025-11-05 14:11:07 +0900

    Create app.js

---
+++ project/TEST/js/app.js
@@ -0,0 +1,5 @@
+document.getElementById('loadImage').addEventListener('click', () => {
+    const img = document.getElementById('outputImage');
+    img.src = '/images/women1.png';
+    img.style.display = 'block';
+});
